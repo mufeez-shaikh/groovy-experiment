6075b5170a686788936f8c49ec35e279ddf3a029
mufeezShaikh <mufeez.shaikh@outlook.com>
2019-02-20 19:39:57 -0700

    first commit

---
+++ src/main/java/com/groovy/groovyexperiment/GroovyExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.groovy.groovyexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groovy/groovyexperiment/GroovyExperimentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.groovy.groovyexperiment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroovyExperimentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
